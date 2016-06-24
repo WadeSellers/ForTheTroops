cdc1f53ab929338ad5c5bd60913e3112cda96bb4
WadeSellers <Waderoni@gmail.com>
2016-06-24 12:30:01 -0600

    extension ajustment

--- ForTheTroops/YLPBusiness+Extension.swift
+++ ForTheTroops/YLPBusiness+Extension.swift
@@ -11,6 +11,6 @@ import YelpAPI
 
 extension YLPBusiness {
     public override var description: String {
-        return "identifier: \(self.identifier) \r\n Business name: \(self.name) \r\n closed: \(self.closed) \r\n imageURL: \(self.imageURL) \r\n displayPhone: \(self.displayPhone) \r\n phone: \(self.phone) \r\n location: \(self.location) \r\n "
+        return "identifier: \(self.identifier) \r\n name: \(self.name) \r\n closed: \(self.closed) \r\n imageURL: \(self.imageURL) \r\n displayPhone: \(self.displayPhone) \r\n phone: \(self.phone) \r\n location: \(self.location) \r\n "
     }
 }
